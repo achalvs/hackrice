898ce6740f43c18a9995071b79046e8542afa340
achalvs <avs@achal.me>
2018-09-07 22:38:26 -0500

    ssr

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
 
-          <title>Init</title>
+          <title>HackRice 8</title>
 
           {this.props.styleTags}
         </Head>
